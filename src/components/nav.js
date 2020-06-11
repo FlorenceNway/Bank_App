859f7d561dffd37d15791862c1384c19b5809377
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-12 00:24:39 +0100

    Navigation

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+	return  <div className="Login">
+                <ul className="nav">
+                    <li>LOGIN</li>
+                    <li>SIGN IN</li>
+                </ul>
+            </div>;
+}
+
+export default Nav;
