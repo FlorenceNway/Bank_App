153b364e7af26098c356366819d1121541180cb3
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-13 00:46:23 +0100

    get auth user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React , {useState,useContext,useEffect} from "react";
 import "./App.css";
-import Nav from './components/nav'
-import Login from './components/login'
+import Nav from './components/Nav'
+import WALLET from './components/Wallet'
+import SIGNUP from './components/SignUp'
+import LOGIN from './components/Login'
+import API from './components/API'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {UserContext} from "./components/UserContext";
+
 
 function App() {
+	const [users, setUsers] = useState([]);
+	//const [user, setUser] = useState(null);
+	const [authUser, setAuthUser] = useState('')
+
+	// return user ? <AuthWebsite/> : <NoAuthWebsite/>
+	// useEffect(() => { 
+    //     API.getUsers().then((users) => {
+    //       setUsers(users);
+    //     });
+    
+    //     return () => {
+    //       console.log("component will unmount!");
+    //     };
+	//   }, []);
+	  
+	
+	const handleSubmit = (prop) => {
+		//e.preventDefault();
+		// const user = users.filter(user => user.email === username && user.password === password)
+		// if(user) {
+		//  setAuthUser("authenticated")
+		// }else {
+		//  setAuthUser("Not Found")
+		// }
+		console.log(prop)
+	}
+
+
 	return <div className="App">
 		<Nav />
-		<Login />
+		<LOGIN onSubmit={handleSubmit} />
+		{/* {user === "authenticated" ? <Wallet/> : <LOGIN/>} } */}
+
+		<Router>
+				<Route path="/" exact component={LOGIN}/>
+				<Route path="/wallet" exact component={WALLET} />
+				<Route path="/signUp" exact component={SIGNUP} />
+		</Router>  
+
 	</div>;
 }
 
