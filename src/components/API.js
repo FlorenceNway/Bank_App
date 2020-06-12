86036f68f169421724227d661f4863e7710b73e3
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-12 13:21:23 +0100

    fetch users

---
+++ src/components/API.js
@@ -0,0 +1,17 @@
+const USERS_URL = "http://localhost:3001/users";
+
+const getUsers = () => fetch(USERS_URL).then((res) => res.json());
+const postUser = (user) =>
+  fetch(USERS_URL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  }).then((res) => res.json());
+
+export default {
+  getUsers,
+  postUser,
+};
