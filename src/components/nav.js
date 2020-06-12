4eaa2b352a4d4abc8d34bf74d6a2cb52c7ad6963
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-12 01:26:37 +0100

    nav list

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './nav.scss';
 
 const Nav = () => {
 	return  <div className="Login">
                 <ul className="nav">
                     <li>LOGIN</li>
+                    <span>/</span>
                     <li>SIGN IN</li>
                 </ul>
             </div>;
