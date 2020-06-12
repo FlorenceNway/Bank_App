3666e67f4763e2508f408a2f9c641ba607a728b1
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-13 00:47:10 +0100

    Sign Up test

---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React, {useContext} from "react";
+import './signUp.scss'
+
+const SingUp = () => {
+  
+	return  <div className="signUp">
+                <h1>Sign up</h1>
+            </div>;
+}
+
+export default SingUp;
