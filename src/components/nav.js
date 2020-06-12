65420cd730c2560b4afc6601ee82efddf6ee461b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-13 00:46:48 +0100

    nav router

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React ,{useState} from "react";
+import Logo from '../images/CH.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LOGIN from './Login'
+import SIGNUP from './SignUp'
+import WALLET from './Wallet'
+import { UserContext } from "./UserContext";
 import './nav.scss';
 
 const Nav = () => {
+    const [user, setUser] = useState("")
+
 	return  <div className="Login">
-                <ul className="nav">
-                    <li>LOGIN</li>
-                    <span>/</span>
-                    <li>SIGN IN</li>
-                </ul>
+                <Router>
+                    <ul className="nav">
+                        <li><img src={Logo} alt="logo"></img></li>
+                        <li>
+                            <Link to="/" ><span>LOGIN</span></Link> / 
+                            <Link to="/signUp"><span>SIGN UP</span></Link>
+                        </li>
+                    </ul>
+                  
+                </Router>  
             </div>;
 }
 
