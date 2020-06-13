1eaf5bf7ce2bc7c13648948eb89955f11a4faf37
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-13 15:49:33 +0100

    useHistory hook

--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,29 @@
 import React,{useState, useEffect, useContext} from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import API from './API';
+import Input from './Form/Input'
 import './login.scss';
-import { UserContext } from './UserContext';
 
 const Login = (props) => {
-    const [users, setUsers] = useState([]);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const history = useHistory();
+    const [fetchUsers, setFetchUsers] = useState([]);
 
-    const performValidation = () => {
-        return username.length > 0 && password.length > 0;
+    const [inputUser, setInputUser] = useState({
+        username: "",
+        password: ""
+    })
+    
+    const changeHandler = (e) => {
+        const {name, value} = e.target
+        setInputUser(
+            {...inputUser,
+             [name]: value}
+        )
     }
-
-    const handleSubmit = (e) => {
-       e.preventDefault();
-       const user = users.filter(user => user.email === username && user.password === password)
-       props.onSubmit(user)
-    }
-
 
     useEffect(() => { 
         API.getUsers().then((users) => {
-          setUsers(users);
+          setFetchUsers(users);
         });
     
         return () => {
@@ -30,20 +31,28 @@ const Login = (props) => {
         };
       }, []);
 
+    console.log('props passed from App', props)
+
+    const handleSubmit = (e) => {
+       e.preventDefault();
+       const {username, password} = inputUser
+       const isValidCredentials = fetchUsers.filter(user => user.email === username && user.password === password)
+       if(isValidCredentials) {
+           // Push 
+           localStorage.userEmail = username;
+           history.push('/wallet')
+        }
+    }
+
+    const isValidForm = inputUser.username && inputUser.password;
 
     return (
         <div className="form">
             <div className="login_title"><span>Login</span></div>
             <form onSubmit={handleSubmit}>
-                <div className="field">
-                    <label className="label">Email</label>
-                    <input className="input" type="email" name="email" onChange={e => setUsername(e.target.value)} required />
-                </div>
-                <div className="field">
-                    <label className="label">Password</label>
-                    <input className="input" type="password" name="password" onChange={e => setPassword(e.target.value)} required />
-                </div>
-                <button disabled={!performValidation()} className={performValidation()? "removeDisable":""} type="submit" >
+                <Input label="Email" type='email' name="username" changeHandler={changeHandler} />
+                <Input label="Password" type='text' name="password" changeHandler={changeHandler} />
+                <button disabled={!isValidForm} className={isValidForm ? "removeDisable":""} type="submit" >
                     Login
                 </button>
                 <Link to="/signUp" ><span>Sign Up</span></Link>
