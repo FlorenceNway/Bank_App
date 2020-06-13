378d24bbbd79ab4c804812e5ce831a524df5928a
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-13 15:51:36 +0100

    nav to Wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,13 +1,27 @@
-import React from "react";
-import './wallet.scss';
-
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./wallet.scss";
 
 const Wallet = () => {
+  const [isRendering, setIsRendering] = useState(false);
+
+  const history = useHistory();
+  useEffect(() => {
+    if (!localStorage.userEmail) {
+      history.push("/");
+    } else {
+      setIsRendering(true);
+    }
+  }, []);
   
-    
-	return  <div className="wallet">
-                <h1> Wallet </h1>
-            </div>
-}
+  return isRendering ? (
+    <div className="wallet">
+      <h1> Wallet </h1>
+    </div>
+  ) : (
+    ""
+  );
+};
 
 export default Wallet;
+
