fb6f59ac59a167d96e8e1263049125a78244ddc6
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-13 15:49:57 +0100

    nav links only

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,27 +1,32 @@
-import React ,{useState} from "react";
-import Logo from '../images/CH.svg';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import LOGIN from './Login'
-import SIGNUP from './SignUp'
-import WALLET from './Wallet'
+import React, { useState } from "react";
+import Logo from "../images/CH.svg";
+import { Route, Link } from "react-router-dom";
+import LOGIN from "./Login";
+import SIGNUP from "./SignUp";
+import WALLET from "./Wallet";
 import { UserContext } from "./UserContext";
-import './nav.scss';
+import "./nav.scss";
 
 const Nav = () => {
-    const [user, setUser] = useState("")
+  const [user, setUser] = useState("");
 
-	return  <div className="Login">
-                <Router>
-                    <ul className="nav">
-                        <li><img src={Logo} alt="logo"></img></li>
-                        <li>
-                            <Link to="/" ><span>LOGIN</span></Link> / 
-                            <Link to="/signUp"><span>SIGN UP</span></Link>
-                        </li>
-                    </ul>
-                  
-                </Router>  
-            </div>;
-}
+  return (
+    <div className="Login">
+      <ul className="nav">
+        <li>
+          <img src={Logo} alt="logo"></img>
+        </li>
+        <li>
+          <Link to="/">
+            <span>LOGIN</span> 
+          </Link> / 
+          <Link to="/signUp">
+            <span>SIGN UP</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Nav;
