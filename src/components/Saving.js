cb3bda666da01bc97dff84cac862e35d8aea9eba
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-15 00:05:07 +0100

    saving data display

--- src/components/Saving.js
+++ src/components/Saving.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import API from './API'
 import LoggedInNav from "./LoggedInNav";
+import './Style/saving.scss'
 
 const Savings = () => {
   const [isRendering, setIsRendering] = useState(false);
+  const [getUsers, setFetchUsers] = useState([]);
+
+  useEffect(() => { 
+    API.getUsers().then((users) => {
+      setFetchUsers(users);
+    });
+  }, []);
+
+  const loggedInUser = getUsers.filter(user => user.email === localStorage.userEmail)
 
   const history = useHistory();
-
   useEffect(() => {
     if (!localStorage.userEmail) {
       history.push("/");
@@ -17,9 +27,40 @@ const Savings = () => {
 
   return isRendering ? (
       
-    <div className="wallet">
+    <div className="saving">
         <LoggedInNav/>
-        <h1> Savings </h1>
+        <div className="profile">
+        <div className='balance'>
+        {loggedInUser.map((user,index)=> (
+            <p key={index}>{user.Saving_balance}</p>))}
+            <p>Balance</p>
+        </div>
+        <div className='saving_buttons'>
+          <button>PAY IN</button>
+          <button>PAY OUT</button>
+        </div>
+      </div>  
+
+      <div className='transBox'>
+        <ul className="transactions">
+          <li className='transTitle'>
+            <p>Transactions</p>
+            <p>Amount</p>
+          </li>
+          <hr/>
+          {loggedInUser.map(user => user.saving_transactions.length?
+          user.saving_transactions.map((transaction,index)=> (
+              <li className='transaction' key={index}>
+                <p>{transaction.transaction}</p>
+                <p>{transaction.debit}<span>£</span>{transaction.amount}</p>
+              </li>
+          )): (<li className='transaction'>
+                  <p>{"No transaction to show!"}</p>
+              </li>
+        ))}
+          
+        </ul>
+      </div>
         
     </div>
   ) : (
