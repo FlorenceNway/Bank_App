d9cc16148c7a32b2a3b7694fec5af4d18036cd7b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-14 02:38:37 +0100

    having the route for each page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useState, useContext, useEffect } from "react";
+import React ,{useState, useEffect} from "react";
 import "./App.css";
 import Nav from "./components/Nav";
+import LoggedInNav from "./components/LoggedInNav";
 import Wallet from "./components/Wallet";
+import Saving from "./components/Saving";
+import Loans from "./components/Loan";
+import Setting from "./components/Setting";
 import Signup from "./components/SignUp";
 import Login from "./components/Login";
-import API from "./components/API";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import { UserContext } from "./components/UserContext";
 
 function App() {
+
   return (
     <div className="App">
-      <Nav />
- 
-      <Route path="/" exact component={Login} />
-      <Route path="/wallet" exact component={Wallet} />
-      <Route path="/signUp" exact component={Signup} />
+    	
+		<Route path="/" exact component={Login} />
+		<Route path="/signUp" exact component={Signup} />
+		<Route path="/wallet" exact component={Wallet} />
+		<Route path="/backArrow" exact component={Login} />
+		<Route path="/signOut" exact component={Login} />
+
+		<Route path="/loan" exact component={Loans} />
+     
     </div>
   );
 }
