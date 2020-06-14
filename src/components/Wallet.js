0b48ddb71aefbf469f8efcbed8d22f18f38945cb
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-14 02:41:16 +0100

    added loggedIn Nav

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import Saving from "./Saving";
+import Loans from "./Loan";
+import Setting from "./Setting";
+import Login from "./Login";
+import { Route} from "react-router-dom";
 import "./wallet.scss";
+import LoggedInNav from "./LoggedInNav";
 
 const Wallet = () => {
   const [isRendering, setIsRendering] = useState(false);
@@ -13,10 +19,17 @@ const Wallet = () => {
       setIsRendering(true);
     }
   }, []);
-  
+
   return isRendering ? (
+      
     <div className="wallet">
-      <h1> Wallet </h1>
+        <LoggedInNav/>
+        <h1> Wallet </h1>
+        
+		<Route path="/saving" exact component={Saving} />
+		<Route path="/loan" exact component={Loans} />
+		<Route path="/setting" exact component={Setting} />
+		<Route path="/signOut" exact component={Login} />
     </div>
   ) : (
     ""
