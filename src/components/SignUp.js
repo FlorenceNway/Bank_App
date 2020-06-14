efd725ca2db4949d8818ddf555b3bc5c2ff37004
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-14 02:53:57 +0100

    added nav bar

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Input from './Form/Input'
 import backArrow from '../images/Arrow.svg'
 import manAvatar from '../images/man_1.svg'
+import Nav from "./Nav";
 import './signUp.scss'
 
 const SingUp = () => {
@@ -31,40 +32,40 @@ const SingUp = () => {
 
     }
 
-	return  <div className="form SignUpform">
-                <div className="signUp_title">
-                <span><Link to="/backArrow"><img src={backArrow} alt="backArrow"></img></Link></span>
-                <span>SIGN UP</span></div>
+    return  (
+            <>
+            <Nav/>
+                <div className="form SignUpform">
+                    <div className="signUp_title">
+                    <span><Link to="/backArrow"><img src={backArrow} alt="backArrow"></img></Link></span>
+                    <span>SIGN UP</span></div>
 
-                <form onSubmit={handleSubmit}>
-                    <Input label="First Name" type='text' name="firstname" changeHandler={changeHandler} />
-                    <Input label="Last Name" type='text' name="lastname" changeHandler={changeHandler} />
-                    <Input label="Email" type='email' name="email" changeHandler={changeHandler} />
-                    <Input label="Password" type='text' name="password" changeHandler={changeHandler} />
-                    <Input label="Confirm Password" type='text' name="confpassword" changeHandler={changeHandler} />
-                    <button type="submit" >
-                        Sign Up
-                    </button>
-                </form>
-                <div className='uploadAvatar'>
-                    
-                    <div className='button'>
-                        <label htmlFor='single'>
-                            Upload
-                        </label>
-                        <input type='file' id='single' onChange={fileChangeHandler} /> 
+                    <form onSubmit={handleSubmit}>
+                        <Input label="First Name" type='text' name="firstname" changeHandler={changeHandler} />
+                        <Input label="Last Name" type='text' name="lastname" changeHandler={changeHandler} />
+                        <Input label="Email" type='email' name="email" changeHandler={changeHandler} />
+                        <Input label="Password" type='text' name="password" changeHandler={changeHandler} />
+                        <Input label="Confirm Password" type='text' name="confpassword" changeHandler={changeHandler} />
+                        <button type="submit" >
+                            Sign Up
+                        </button>
+                    </form>
+                    <div className='uploadAvatar'>
+                        
+                        <div className='button'>
+                            <label htmlFor='single'>
+                                Upload
+                            </label>
+                            <input type='file' id='single' onChange={fileChangeHandler} /> 
+                        </div>
+                        <div>
+                            <img src={manAvatar}></img>
+                        </div> 
                     </div>
-                    {/* <div>
-                        <p>Upload avatar</p>
-                        <div className='fileInput'>Upload<input type="file" name="fileInput" style={{height:0, overflow:'hidden'}}/></div>
-                    </div> */}
-
-                    <div>
-                        <img src={manAvatar}></img>
-                    </div> 
+                    
                 </div>
-                
-            </div>
+            </>
+    )
 }
 
 export default SingUp;
