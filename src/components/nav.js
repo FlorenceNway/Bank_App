a39dd49e3cfc69b51b838ccb46dff6fc58763f48
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-15 16:51:35 +0100

    combine with setting component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from "../images/CH.svg";
 import { NavLink } from "react-router-dom";
+import Setting from './Setting'
 import "./Style/nav.scss";
 import LinksBeforeLogin from "./LinksBeforeLogIn";
 import LinksAfterLogIn from './LinksAfterLogIn';
 
 const Nav = () => {
+  const [isHidden, setIsHidden] = useState(true)
 
-  return (
+  const clickSetting = () => {
+    setIsHidden(!isHidden)
+  }
+
+  return (<>
     <div className="Login LoginNav">
       <ul className="login_nav nav">
         <li>
           <img src={Logo} alt="logo"></img>
         </li>
-        {localStorage.userEmail ? <LinksAfterLogIn/> : <LinksBeforeLogin/>}
+        {localStorage.userEmail ? <LinksAfterLogIn clickSetting={clickSetting}/> : <LinksBeforeLogin/>}
       </ul>
     </div>
-  );
+    
+    <section className="settingSection">
+      {!isHidden && <Setting />}
+    </section>
+    
+  </>);
 };
 
 export default Nav;
