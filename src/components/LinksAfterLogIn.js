2f1f2226fceb332b2b084356869230acd1085259
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-15 16:46:05 +0100

    customized link for setting

--- src/components/LinksAfterLogIn.js
+++ src/components/LinksAfterLogIn.js
@@ -2,18 +2,19 @@ import React,{useState} from "react";
 import { NavLink } from "react-router-dom";
 import "./Style/nav.scss";
 
-const LinksAfterLogIn = () => {
+const LinksAfterLogIn = ({clickSetting}) => {
 
   const signOut = () => {
     localStorage.removeItem('userEmail')
   }
 
   const [menu, setMenu] = useState({
-    links: [{id: 1,name:'wallet'},{id: 2,name:'loans'}, {id: 3,name:'savings'},{id: 4,name:'setting'}, {id: 5,name:'signout'}]
+    links: [{id: 1,name:'wallet'},{id: 2,name:'loans'}, {id: 3,name:'savings'},{id: 4,name:'Setting'}, {id: 5,name:'signout'}]
   })
 
   return  <li>
             {menu.links.map((link) =>(
+              link.name == "Setting" ? <span className={'settingTab'} onClick={clickSetting}>{link.name}</span> :
             <NavLink to={`/${link.name}`} activeClassName={"active"} key={link.id}>
                 {link.name === "signout"?
                 <span onClick={signOut}>{link.name}</span> : <span>{link.name}</span> 
