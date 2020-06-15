558ca590d7e53388ab7878356f299bc977154e60
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-15 16:52:02 +0100

    to show the value of logged in user in input fileds

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,25 +1,102 @@
-import React, { useEffect, useState } from "react";
+import React,{useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import Input from './Form/Input'
+import backArrow from '../images/Arrow.svg'
+import manAvatar from '../images/man_1.svg'
+import API from './API'
 import { useHistory } from "react-router-dom";
-import LoggedInNav from "./LoggedInNav";
+import './Style/setting.scss'
 
 const Setting = () => {
-  const [isRendering, setIsRendering] = useState(false);
+  const [isHidden, setIsHidden] = useState(false)
 
-  const history = useHistory();
+  const clickSetting = () => {
+    setIsHidden(!isHidden)
+  }
 
-  useEffect(() => {
-    if (!localStorage.userEmail) {
-      history.push("/");
-    } else {
-      setIsRendering(true);
-    }
+  const [users, setUsers] = useState([]);
+  const [updateUser, setUpdateUser] = useState([])
+  
+  useEffect(() => { 
+    API.getUsers().then((users) => {
+      setUsers(users);
+    });
   }, []);
 
-  return isRendering ? (
+  const loggedInUser = users.filter(user => user.email === localStorage.userEmail)
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+    const id = loggedInUser[0].id
+    API.patchUser(id,updateUser)
       
-    <div className="wallet">
-        <LoggedInNav/>
-        <h1> Setting </h1>
+    alert("Your account has been updated!")
+  }
+  
+  const changeHandler = (e) => {
+      const {name, value} = e.target
+      setUpdateUser(
+          {...updateUser,
+            [name]: value}
+      )
+  }
+
+  const uploadFileHandler = (e) => {
+    let imgPath = e.target.value.split("\\")
+
+    setUpdateUser({...updateUser,
+        avatar: imgPath[2]})
+  }
+
+  
+  return !isHidden && loggedInUser.length ? (
+    <div className="setting">
+        <div className="form SignUpform">
+            <div className="signUp_title">
+              <span onClick={clickSetting}><img src={backArrow} alt="backArrow"></img></span>
+              <span>Settings</span>
+            </div>
+            <div className="box">
+              <div>
+                <p>Block account</p>
+                <span className='on'>On</span>
+                <label className="switch">
+                  <input type="checkbox" value="Off"/><span className="slider"></span>
+                </label>
+                <span className='off'>Off</span>
+              </div>
+              <div>
+                <p>Round expenses and put to savings account</p>
+                <span className='on'>On</span>
+                <label className="switch">
+                  <input type="checkbox" value="Off"/><span className="slider"></span>
+                </label>
+                <span className="off">Off</span>
+              </div> 
+            </div>
+            <form onSubmit={handleSubmit}>
+                <Input label="First Name" type='text' name="firstname" changeHandler={changeHandler} value={loggedInUser[0].firstname} />
+                <Input label="Last Name" type='text' name="lastname" changeHandler={changeHandler} value={loggedInUser[0].lastname}/>
+                <Input label="Email" type='email' name="email" changeHandler={changeHandler} value={loggedInUser[0].email}/>
+                <Input label="Password" type='text' name="password" changeHandler={changeHandler} value={loggedInUser[0].password}/>
+                <Input label="Confirm Password" type='text' name="confpassword" changeHandler={changeHandler} value={loggedInUser[0].password}/>
+                
+                <div className='uploadAvatar'>
+                    <div className='button'>
+                        <label htmlFor='single'>
+                            Upload
+                        </label>
+                        <input type='file' id='single' onChange={uploadFileHandler} /> 
+                    </div>
+                    <div>
+                        <img src={manAvatar}></img>
+                    </div> 
+                </div>
+
+                <button type="submit" className="saveBtn"> Save </button>
+            </form>
+        </div>
         
     </div>
   ) : (
