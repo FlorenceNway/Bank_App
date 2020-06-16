67af92af604b19b4750383bfac397160b9286991
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-16 03:25:16 +0100

    buttons to work , pay In , pay Out functions

--- src/components/Saving.js
+++ src/components/Saving.js
@@ -7,15 +7,25 @@ import './Style/saving.scss'
 
 const Savings = () => {
   const [isRendering, setIsRendering] = useState(false);
-  const [getUsers, setFetchUsers] = useState([]);
+  const [Users, setUsers] = useState([]);
+  const [user, setUser] = useState(null)
+  const [value, setValue] = useState(0)
+  const [addMinus, setAddMinus] = useState(null)
+  const [payInActive, setPayInActive] = useState(false)
+  const [payOutActive, setPayOutActive] = useState(false)
 
-  useEffect(() => { 
+  useEffect(() => {
     API.getUsers().then((users) => {
-      setFetchUsers(users);
+      setUsers(users);
+
+      const loggedInUser = Users.find(
+        (user) => user.email === localStorage.userEmail
+      );
+      setUser(loggedInUser);
     });
   }, []);
 
-  const loggedInUser = getUsers.filter(user => user.email === localStorage.userEmail)
+  const loggedInUser = Users.filter(user => user.email === localStorage.userEmail)
 
   const history = useHistory();
   useEffect(() => {
@@ -26,21 +36,60 @@ const Savings = () => {
     }
   }, []);
 
+  const getValuetoTransfer = (e) => {
+    setValue(e.target.value)
+  }
+
+  const payIn = () => {
+    setAddMinus('+')
+    setPayInActive(true)
+    setPayOutActive(false)
+  }
+
+  const payOut = () => {
+    setAddMinus('-')
+    setPayOutActive(true)
+    setPayInActive(false)
+  }
+
+  const transfer = () => {
+    if(addMinus == '+') {
+      loggedInUser[0].Saving_balance += parseInt(value)
+      setUser({...user,Saving_balance:loggedInUser[0].Saving_balance})
+    
+    }else {
+      loggedInUser[0].Saving_balance -= parseInt(value)
+      setUser({...user,Saving_balance:loggedInUser[0].Saving_balance})
+    }
+    
+    API.patchUser(loggedInUser[0].id,user)
+  }
+
   return isRendering ? (
       
-    <div className="saving">
+    <div className="loan saving">
         <Nav/>
-        <div className="profile">
-        <div className='balance'>
-        {loggedInUser.map((user,index)=> (
-            <p key={index}>{user.Saving_balance}</p>))}
-            <p>Balance</p>
-        </div>
-        <div className='saving_buttons'>
-          <button>PAY IN</button>
-          <button>PAY OUT</button>
-        </div>
-      </div>  
+        <section className={'balTranfSection'}>
+            <div className="profile">
+              <div className='balance'>
+                { !user ?
+                  loggedInUser.map((user,index)=> (
+                      <p key={index}>{user.Saving_balance}</p>)): <p>{user.Saving_balance}</p>} 
+                      <p>Balance</p>
+              </div>
+              <div className='saving_buttons'>
+                <button onClick={payIn} className={payInActive?"payInactive":""}>PAY IN</button>
+                <button onClick={payOut} className={payOutActive?"payOutactive":""}>PAY OUT</button>
+              </div>
+            </div>  
+              <hr/>
+              {addMinus? 
+              <div className="transfer">
+                <span>£</span><input type='text' onChange={getValuetoTransfer}/>
+                <button onClick={transfer}>Transfer</button>
+              </div>: ""}
+            
+        </section>   
 
       <div className='transBox'>
         <ul className="transactions">
