11a3fec8244bd5eba48b957e9fb7eefc7ba72d21
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-16 06:01:59 +0100

    use Context for passing block feature ON/OFF

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React, { useContext,useState, useMemo}  from "react";
 import "./App.css";
 import Wallet from "./components/Wallet";
 import Saving from "./components/Saving";
@@ -11,17 +11,23 @@ import { UserContext } from "./components/UserContext";
 
 function App() {
 
+	const [val, setVal] = useState(null);
+	//const providerValue = useMemo(() => ({val, setVal}), [val, setVal])	
+
   return (
     <div className="App">
 		<Route path="/" exact component={Login} />
 		<Route path="/signUp" exact component={Signup} />
 		<Route path="/backArrow" exact component={Login} />
 		
-		<Route path="/wallet" exact component={Wallet} />
-		<Route path="/loans" exact component={Loans} />
-		<Route path="/savings" exact component={Saving} />
-      	<Route path="/setting" exact component={Setting} />
-		<Route path="/signOut" exact component={Login} />
+		<UserContext.Provider value={{val, setVal}}>
+			<Route path="/wallet" exact component={Wallet} />
+			<Route path="/loans" exact component={Loans} />
+			<Route path="/savings" exact component={Saving} />
+			<Route path="/setting" exact component={Setting} />
+			<Route path="/signOut" exact component={Login} />
+		</UserContext.Provider>
+		
     </div>
   );
 }
