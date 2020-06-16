5eedddd0eeac301b10cadf9fb23eb6c428ad7888
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-16 03:34:18 +0100

    save update data in db

--- src/components/Loan.js
+++ src/components/Loan.js
@@ -62,7 +62,7 @@ const Loan = () => {
       setUser({...user,Loan_balance:loggedInUser[0].Loan_balance})
     }
     
-    API.patchUser(loggedInUser[0].id,user)
+    API.patchUser(loggedInUser[0].id,loggedInUser[0])
   }
 
   return isRendering ? (
